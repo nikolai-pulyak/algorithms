c668694f10bd3400170c4f9026707621a5581990
nikolay <nikolay.puliak@outlook.com>
2022-01-27 10:54:00 +0300

    binarySearch

--- binary-search/binary.search.ts
+++ binary-search/binary.search.ts
@@ -1,8 +1,17 @@
 type ItemType = string|number
-export function binarySearch(arr: ItemType[], val: ItemType[]): ItemType|null {
-  let maxIndex = arr.length - 1, minIndex = 0, avgIndex, midd
-  while(maxIndex > 0) {
-    avgIndex = Math.floor((maxIndex + min)
-    midd = arr[Math]
+export function binarySearch(arr: ItemType[], val: ItemType[]): number {
+  let maxIndex = arr.length - 1, minIndex = 0, middIndex = -1, midd
+  while(maxIndex > middIndex) {
+    middIndex = Math.floor(maxIndex / 2 + minIndex / 2)
+    midd = arr[middIndex]
+    
+    if(midd === val)
+      return middIndex
+    
+    if(midd < val)
+      minIndex = middIndex + 1
+    else
+      maxIndex = middIndex - 1
   }
+  return -1
 }
